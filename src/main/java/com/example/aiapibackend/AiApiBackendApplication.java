b99881f44179f3e42054b2db4f3c55f51f97037c
Yasi1095 <yasi1095@stud.kea.dk>
2023-11-06 13:45:39 +0100

    dkd

---
+++ src/main/java/com/example/aiapibackend/AiApiBackendApplication.java
@@ -0,0 +1,16 @@
+package com.example.aiapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiApiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiApiBackendApplication.class, args);
+
+        //blablabalajsj
+    }
+
+
+}
---
+++ src/test/java/com/example/aiapibackend/AiApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aiapibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiApiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
